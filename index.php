7615133e56ed0d20703c4853f1fc4a311b370d31
Papa Charlie <p.c.1@mailcity.com>
2018-05-11 20:36:33 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>stage1: HNG Internship 4</title>
+  <link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+  <div class="date">
+    <?php
+      echo date("l, F d, Y ");
+    ?> 
+  </div>
+
+  <div class="time">
+    <?php
+      echo date(" H:i");
+    ?>
+    <p class="timez">GMT </p>
+  </div>
+
+
+  <div class="intro">
+    <p>HNG Internship 4 </p>
+
+  </div>
+
+</body>
+
+</html>
+
